bd9cc6458cad41b417e4f3c9067afcd84f10064c
sqweek <sqweek@gmail.com>
2014-03-18 09:26:18 +0800

    Basic fluidsynth bindings for Go
    Supports:
     * Changing settings
     * Loading soundfont
     * Changing program for channel
     * Note on/off
     * Fluidsynth write interface (ie. application controls audio)
    
    NOTE: fluidsynth audio driver not yet supported

---
+++ synth.go
@@ -0,0 +1,81 @@
+package fluidsynth
+
+// #cgo pkg-config: fluidsynth
+// #include <fluidsynth.h>
+// #include <stdlib.h>
+import "C"
+import "reflect"
+
+import (
+	"os"
+	"fmt"
+	"unsafe"
+)
+
+type Synth struct {
+	csettings *C.fluid_settings_t
+	csynth *C.fluid_synth_t
+	cdriver *C.fluid_audio_driver_t
+}
+
+func NewSynth(settings map[string]interface{}) *Synth {
+	csettings, _ := C.new_fluid_settings()
+	for key, value := range settings {
+		ckey := C.CString(key)
+		switch value := value.(type) {
+		case string:
+			cval := C.CString(value)
+			C.fluid_settings_setstr(csettings, ckey, cval)
+			C.free(unsafe.Pointer(cval))
+		case int32:
+			C.fluid_settings_setint(csettings, ckey, C.int(value))
+		case float64:
+			C.fluid_settings_setnum(csettings, ckey, C.double(value))
+		default:
+			fmt.Fprintf(os.Stderr, "NewSynth: ignoring setting %s: unhandled type %T\n", key, value)
+		}
+		C.free(unsafe.Pointer(ckey))
+	}
+	csynth := C.new_fluid_synth(csettings)
+	//cdriver := C.new_fluid_audio_driver(csettings, csynth)
+	return &Synth{csettings, csynth, nil}
+}
+
+func (s *Synth) SFLoad(path string, resetPresets bool) int {
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+	creset := C.int(1)
+	if !resetPresets {
+		creset = 0
+	}
+	cfont_id, _ := C.fluid_synth_sfload(s.csynth, cpath, creset)
+	return int(cfont_id)
+}
+
+/* XXX can this be run automatically on gc? */
+func (s *Synth) Delete() {
+	//C.delete_fluid_audio_driver(s.cdriver)
+	C.delete_fluid_synth(s.csynth)
+	C.delete_fluid_settings(s.csettings)
+}
+
+func (s *Synth) NoteOn(channel, note, velocity int) {
+	C.fluid_synth_noteon(s.csynth, C.int(channel), C.int(note), C.int(velocity))
+}
+
+func (s *Synth) NoteOff(channel, note int) {
+	C.fluid_synth_noteoff(s.csynth, C.int(channel), C.int(note))
+}
+
+func (s *Synth) ProgramChange(channel, program int) {
+	C.fluid_synth_program_change(s.csynth, C.int(channel), C.int(program))
+}
+
+func (s *Synth) WriteFrames_int16(nf int) []int16 {
+	buf := make([]int16, nf*2)
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&buf))
+	cbuf := unsafe.Pointer(hdr.Data)
+	C.fluid_synth_write_s16(s.csynth, C.int(nf), cbuf, 0, 2, cbuf, 1, 2)
+	return buf
+}
+
